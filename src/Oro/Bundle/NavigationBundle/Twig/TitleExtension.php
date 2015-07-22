ece8e64b1f1c0e98925be271ff8dc01a7ce72586
Filips Alpe <falpe@evolutiongaming.com>
2015-07-22 10:06:01 +0300

    Fix pinbar and favorite menu titles

--- src/Oro/Bundle/NavigationBundle/Twig/TitleExtension.php
+++ src/Oro/Bundle/NavigationBundle/Twig/TitleExtension.php
@@ -35,8 +35,6 @@ class TitleExtension extends \Twig_Extension
     {
         return array(
             'oro_title_render' => new \Twig_Function_Method($this, 'render'),
-            'oro_title_render_short' => new \Twig_Function_Method($this, 'renderShort'),
-            'oro_title_render_serialized' => new \Twig_Function_Method($this, 'renderSerialized'),
         );
     }
 
@@ -63,29 +61,6 @@ class TitleExtension extends \Twig_Extension
         return $this->titleService
             ->setData($this->getTitleData())
             ->render(array(), $titleData, null, null, true);
-    }
-
-    /**
-     * Renders short title
-     *
-     * @param null $titleData
-     * @return string
-     */
-    public function renderShort($titleData = null)
-    {
-        return $this->titleService
-            ->setData($this->getTitleData())
-            ->render(array(), $titleData, null, null, true, true);
-    }
-
-    /**
-     * Returns json serialized data
-     *
-     * @return string
-     */
-    public function renderSerialized()
-    {
-        return $this->titleService->setData($this->getTitleData())->getSerialized();
     }
 
     /**
