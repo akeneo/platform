c3bd7b710f36eff6e789600936b4e683daf00e7b
pierallard <pierallard@gmail.com>
2015-10-08 17:43:58 +0200

    PIM-4940: Filter available UI locales from minimal translated progress

--- src/Oro/Bundle/LocaleBundle/Form/Type/LocaleType.php
+++ src/Oro/Bundle/LocaleBundle/Form/Type/LocaleType.php
@@ -5,19 +5,47 @@ namespace Oro\Bundle\LocaleBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Intl\Intl;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\Translator;
 
 class LocaleType extends AbstractType
 {
+    /** @var Translator */
+    protected $translator;
+
+    /** @var float */
+    protected $minTranslationProgress;
+
+    /**
+     * @param Translator $translator
+     * @param float      $minTranslationProgress
+     */
+    public function __construct(Translator $translator, $minTranslationProgress)
+    {
+        $this->translator             = $translator;
+        $this->minTranslationProgress = (float) $minTranslationProgress;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(
-            array(
-                'choices' => Intl::getLocaleBundle()->getLocaleNames('en'),
-            )
-        );
+        $availableLocales = Intl::getLocaleBundle()->getLocaleNames('en');
+        $locales = [];
+
+        $mainReference = count($this->translator->getMessages('en'), COUNT_RECURSIVE);
+        $minTranslations = $mainReference * $this->minTranslationProgress;
+
+        foreach ($availableLocales as $code => $locale) {
+            if (preg_match('/^[a-z]{2}$/i', $code)) {
+                $reference = count($this->translator->getMessages($code), COUNT_RECURSIVE);
+                if ($reference >= $minTranslations) {
+                    $locales[$code] = $locale;
+                }
+            }
+        }
+
+        $resolver->setDefaults(['choices' => $locales]);
     }
 
     /**
